003a73c6dba83d94470c6427b8cf3e006be2ec2d
André Paraíso Ferreira <andre.paraisoferreira@siemens.com>
2018-06-29 15:13:59 +0100

    FIX way of getting heroku port was wroung

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
   /**
    * Get the port envirement variable
    */
-  const port = process.env.port || 3000;
+  const port = process.env.PORT || 3000;
   console.log(process.env);
   /**
    * Initialization of express
